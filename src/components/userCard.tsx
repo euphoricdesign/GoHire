3eb2d829f16b68f415ca2361f62bf82b45492974
NachTorr <nachtorrejon23@gmail.com>
2024-05-28 17:33:42 -0300

    cards actualizadas info y colores

--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { GoHeartFill } from "react-icons/go";
 import { IUser } from "@/types";
 
-const UserCard = ({ id, name, city, description, skills }: IUser) => {
+const UserCard = ({ profileImg, name, lastName, country, city, description, skills }: IUser) => {
   const [FavClicked, setFavClicked] = useState(false);
 
   const handleFavClick = () => {
@@ -12,12 +12,12 @@ const UserCard = ({ id, name, city, description, skills }: IUser) => {
   };
 
   return (
-    <div className="flex items-center justify-between w-80 flex-col m-4 shadow-2xl">
-      <div className="bg-indigo-600 rounded-t-md text-slate-100 pt-2 pb-2 relative w-full text-center h-96 flex flex-col justify-around">
+    <div className="flex items-center justify-between w-80 flex-col m-4 shadow-2xl scale-90 hover:scale-95 transition-all duration-300">
+      <div className="bg-[#5055C3] rounded-t-md text-slate-100 pt-2 pb-2 relative w-full text-center h-[26rem] flex flex-col justify-around">
         <div className="flex justify-end px-5 mt-2">
           <Image
-            className="rounded-full p-2 border-2 border-slate-400"
-            src={""}
+            className="rounded-full h-52 w-52 p-2 border-2 border-slate-400"
+            src={profileImg}
             alt="perfil"
             width={200}
             height={200}
@@ -38,17 +38,21 @@ const UserCard = ({ id, name, city, description, skills }: IUser) => {
                 className="bg-white pt-1 px-1 pb-[2px] rounded-full border-2 border-indigo-900 hover:border-indigo-300 transition-all duration-300">
                 <GoHeartFill
                   size={30}
-                  className="text-indigo-900 hover:text-indigo-300 transition-all duration-300"
+                  className="text-indigo-600 hover:text-indigo-600 transition-all duration-300"
                 />
               </button>
             )}
           </span>
         </div>
-        <h3 className="font-bold text-lg text-white my-1">{name}</h3>
-        <h6 className="text-white font-bold">{city}</h6>
+        <h3 className="font-bold text-lg text-white my-1">
+          {name} {lastName}
+        </h3>
+        <h6 className="text-white font-bold">
+          {city}, {country}
+        </h6>
         <p className="text-sm	leading-5 text-white mt-1 mb-4">{description}</p>
         <div className="">
-          <button className="bg-white border mr-2 w-28 text-indigo-600 border-slate-300 font-medium py-2 px-4 rounded hover:bg-indigo-300 transition-all duration-300">
+          <button className="bg-white border mr-2 w-28 text-indigo-600 border-slate-300 font-medium py-2 px-4 rounded hover:bg-[#A1ABFF] transition-all duration-300">
             Message
           </button>
           <button className="bg-transparent border w-28 border-slate-100 font-medium py-2 px-6 rounded text-slate-100 hover:bg-indigo-300 transition-all duration-300">
@@ -56,7 +60,7 @@ const UserCard = ({ id, name, city, description, skills }: IUser) => {
           </button>
         </div>
       </div>
-      <div className="bg-indigo-700  px-4 py-2 font-bold rounded-b-md h-28 w-full text-slate-100 ">
+      <div className="bg-[#4347A4] px-4 py-2 font-bold rounded-b-md h-28 w-full text-slate-100 border-t border-slate-100">
         <h6 className="text-center">Skills</h6>
         <div className="overflow-auto h-[5rem]">
           <ul className="list-none m-0 p-0 text-sm flex flex-wrap">
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,9 @@
 export interface IUser {
   id: number;
+  profileImg: string;
   name: string;
+  lastName: string;
+  country: string;
   city: string;
   description: string;
   skills: string[];
