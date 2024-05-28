d0a8b29b1e16b3c36adc44fe38674bb49da8149f
Merlina <noahchukita@gmail.com>
2024-05-28 13:03:46 -0300

    Cambiando barra separadora entre links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className='flex items-center gap-3 text-sm'>
-          <p className='hidden md:block'><span>Sign in |</span> <span>Dashboard</span></p>
+          <div className='hidden md:flex md:items-center'><a className="">Sign in</a> <div className='w-px h-4 bg-gray-600 mx-1.5'></div> <a>Dashboard</a></div>
           <button className='mt-0 mb-5 text-sm border-none w-28 p-2.5 h-10 rounded text-white font-medium bg-[#5049E5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0 md:block hidden'>Post a job</button>
         </div>
     </header>
