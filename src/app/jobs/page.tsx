6926ae6bf9129dbaf061a6a261c97ea264b5ffc3
alejo saez <alejosaezgebicki@gmail.com>
2024-05-29 12:26:07 -0300

    modularizado de archivos moqueados

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,8 +1,8 @@
-"use client"
+// "use client"
 
 import React from 'react';
 import CardJobs from '../../components/CardJobs/CardJobs';
-import jobData from '../../components/CardJobs/jobs.json';
+import jobData from '../../utils/jobs.json';
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
