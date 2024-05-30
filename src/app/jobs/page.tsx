8ec7f92d36d17a8577d69812bcad63d39d46de4e
Merlina <noahchukita@gmail.com>
2024-05-30 10:25:45 -0300

    Ajustando estilos

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -5,10 +5,13 @@ import CardJobs from '../../components/CardJobs/CardJobs';
 import jobData from '../../utils/jobs.json';
 const Home: React.FC = () => {
   return (
-    <div className="container mx-auto p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-      {jobData.users.map((user, index) => (
-        <CardJobs  key={index} {...user} />
-      ))}
+    <div className="mt-[100px] px-[124px]">
+      
+      <div className='container mx-auto p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+        {jobData.users.map((user, index) => (
+          <CardJobs  key={index} {...user} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import "../../utils/Navbar.css";
+import { usePathname } from 'next/navigation'
+
 
 interface AnchorProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   style?: React.CSSProperties & { "--i"?: number };
@@ -9,6 +11,9 @@ interface AnchorProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
 
 const Navbar: React.FC = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
+  
+  const pathname = usePathname()
+  console.log(pathname !== '/')
 
   useEffect(() => {
     const handleScroll = () => {
@@ -59,12 +64,12 @@ const Navbar: React.FC = () => {
 
       <div className="flex items-center gap-3 text-sm">
         <div className="hidden md:flex md:items-center">
-          <a className={`${scrollPosition > 0 ? "text-gray-600" : "text-white"}`}>Sign in</a>{" "}
+          <a className={`${scrollPosition > 0 || pathname !== '/' ? "text-gray-600" : "text-white"}`}>Sign in</a>{" "}
           <div
             className={`w-px h-4 ${
-              scrollPosition > 0 ? "bg-gray-600" : "bg-white"
+              scrollPosition > 0 || pathname !== '/' ? "bg-gray-600" : "bg-white"
             } mx-1.5`}></div>{" "}
-          <a className={`${scrollPosition > 0 ? "text-gray-600" : "text-white"}`}>Dashboard</a>
+          <a className={`${scrollPosition > 0 || pathname !== '/' ? "text-gray-600" : "text-white"}`}>Dashboard</a>
         </div>
         <button className="mt-0 mb-5 text-sm border-none w-28 p-2.5 h-10 rounded text-white font-medium bg-[#5049E5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0 md:block hidden">
           Post a job
