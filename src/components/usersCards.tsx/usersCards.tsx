ecb565d05b6d588f315ca461e5dc9aa2d4e1479d
Merlina <noahchukita@gmail.com>
2024-05-31 11:20:32 -0300

    Corrección estilos user card

--- src/components/usersCards.tsx/usersCards.tsx
+++ src/components/usersCards.tsx/usersCards.tsx
@@ -31,17 +31,17 @@ const UsersCards = ({ users }: { users: IUser[] }) => {
   };
 
   return (
-    <div className="relative flex overflow-x-hidden h-screen">
+    <div className="relative block">
       {showDescription && (
         <div
           className="fixed inset-0 bg-gray-700 bg-opacity-50 z-[101]"
           onClick={() => setShowDescription(false)}></div>
       )}
-      <div className="flex justify-center items-center flex-wrap z-30">
+      <div className="flex flex-wrap gap-[20px]">
         {users.map((user) => (
           <div
             key={user.id}
-            className="scale-90 hover:scale-95 transition-all duration-300 relative"
+            className="hover:scale-95 transition-all duration-300 relative"
             onClick={() => handleDescription(user)}>
             <UserCard {...user} onClick={() => handleDescription(user)} />
           </div>
