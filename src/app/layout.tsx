c7b82ab1fc00479b71ee43acddbfd1e4d7c35684
alejo saez <alejosaezgebicki@gmail.com>
2024-06-01 13:59:38 -0300

    Se corrigio el error del layout con children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <div>{children}</div>
+        {children}
       </body>
     </html>
   );
