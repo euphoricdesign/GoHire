e7f588350bd7a99fc5c10d36bbd5b2b59c102d74
Merlina <noahchukita@gmail.com>
2024-06-02 20:52:54 -0300

    searchbar mobile

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,18 +3,18 @@ import { GoLocation, GoPencil } from "react-icons/go";
 2
 const SearchBar = () => {
   return (
-    <form className='w-[730px] bg-white py-[12px] px-[16px] flex gap-4 items-center justify-center rounded shadow-custom'>
-       <div className='w-[calc(50% - 63px)] flex items-center'>
+    <form className='md:w-[730px] bg-white py-[12px] px-[16px] md:flex gap-4 items-center justify-center rounded shadow-custom'>
+       <div className='w-[calc(50% - 63px)] flex items-center mobile:mb-[15px]'>
           <label htmlFor=""><GoPencil className='text-gray-600' /></label>
           <input className='py-[0.6rem] px-4 focus:outline-none' type="text" placeholder='Keywords' />
        </div>
        <div></div>
-       <div className='w-[calc(50% - 63px)] flex items-center'>
+       <div className='w-[calc(50% - 63px)] flex items-center mobile:mb-[15px]'>
           <label htmlFor=""><GoLocation className='text-gray-600' /></label>
           <input className='py-[0.6rem] px-4 focus:outline-none' type="text" placeholder='Location' />
        </div>
        <div>
-        <button className='mb-0 text-sm border-none w-28 p-2.5 h-10 rounded text-white font-medium bg-[#5049E5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 '>Buscar</button>
+        <button className='mb-0 text-sm border-none md:w-28 mobile:w-full p-2.5 h-10 rounded text-white font-medium bg-[#5049E5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 '>Buscar</button>
        </div>
     </form>
   )
