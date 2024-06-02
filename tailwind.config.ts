eb7ebe8765ccc4af09e5d6d3c49ced2d3c411097
Merlina <noahchukita@gmail.com>
2024-06-02 20:54:41 -0300

    Definiendo breakpoints taildwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,21 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "violetBg": 'url("https://rekroot.themes.zone/wp-content/uploads/2021/07/home1_back.png")'
       },
+      screens: {
+        // Breakpoints predefinidos
+        'sm': '640px',
+        'md': '768px', 
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        // Breakpoints personalizados
+        'tablet': '900px', // Dispositivos con un ancho de 900px o superior
+        'desktop': '1200px', // Dispositivos con un ancho de 1200px o superior
+        // Breakpoints para dispositivos móviles modernos
+        'mobile': '360px', // Dispositivos móviles pequeños
+        'phablet': '480px', // Dispositivos móviles medianos  
+        'phone': '580px', // Dispositivos móviles grandes
+      },
       boxShadow: {
         'custom': '0px 0px 30px 0px rgba(98, 91, 229, 0.15)',
       },
