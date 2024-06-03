29d4a0f8c450c5146c1b0c00790f543232a8d000
Merlina <noahchukita@gmail.com>
2024-06-03 19:05:38 -0300

    Cambios user card

--- src/components/userCard/userCard.tsx
+++ src/components/userCard/userCard.tsx
@@ -41,20 +41,20 @@ const UserCard = ({
   const truncatedDescription = truncateText({ text: description, maxLength: 100 });
 
   return (
-    <div className="cursor-pointer bg-[ghostwhite] mobile:w-full" onClick={onClick}>
-      <div className="flex items-center justify-between w-[25.8rem] flex-col shadow-md transition-all duration-300 border rounded-xl mobile:w-full">
+    <div className="cursor-pointer bg-[ghostwhite] mobile:w-full md:w-[25.8rem]" onClick={onClick}>
+      <div className="flex items-center justify-between md:w-[25.8rem] flex-col shadow-md transition-all duration-300 border rounded-xl mobile:w-full">
         <div className="w-full border-b border-gray-300">
           <div className="flex justify-between w-full p-5">
             <div className="flex flex-col justify-between">
               <div className="flex items-center">
                 <button
                   onClick={handleButtonClick}
-                  className="bg-white border mr-2 w-28 text-[#5049e5] border-slate-300 font-medium py-2 px-4 rounded-xl hover:bg-[#93B4FF] transition-all duration-300">
+                  className="bg-white border mr-2 w-28 text-[#3C65F5] border-slate-300 font-medium py-2 px-4 rounded-xl hover:bg-[#93B4FF] transition-all duration-300">
                   Message
                 </button>
                 <button
                   onClick={handleButtonClick}
-                  className="bg-[#5049e5] text-white text-center px-5 py-2 rounded-xl mr-2 hover:opacity-80 transition-all duration-300">
+                  className="bg-[#3C65F5] text-white text-center px-5 py-2 rounded-xl mr-2 hover:opacity-80 transition-all duration-300">
                   Hire
                 </button>
                 {FavClicked ? (
@@ -63,7 +63,7 @@ const UserCard = ({
                   </button>
                 ) : (
                   <button onClick={handleFavClick} className="transition-all duration-300 mobile:hidden md:block">
-                    <GoHeartFill className="text-[#5049e5] bg-white hover:bg-[#D5E2FF] transition-all duration-300 size-10 border-2 p-1 border-[#5049e5] rounded-full" />
+                    <GoHeartFill className="text-[#3C65F5] bg-white hover:bg-[#D5E2FF] transition-all duration-300 size-10 border-2 p-1 border-[#3C65F5] rounded-full" />
                   </button>
                 )}
               </div>
@@ -78,7 +78,7 @@ const UserCard = ({
             </div>
             <div>
               <Image
-                className="rounded-full p-[1px] border-2 border-[#5049e5]"
+                className="rounded-full p-[1px] border-2 border-[#3C65F5]"
                 src={profileImg}
                 alt="perfil"
                 width={80}
