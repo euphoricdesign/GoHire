71010335ae35a7011dcd68cff9e40bd8cc886b07
Merlina <noahchukita@gmail.com>
2024-06-04 13:41:48 -0300

    Agregando tipado category card

--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 
-const CategoryCard = ({title, vacancies, img}) => {
+interface CategoryCardProps {
+  title: string,
+  vacancies: string,
+  img: string
+}
+
+const CategoryCard: React.FC<CategoryCardProps> = ({title, vacancies, img}) => {
   return (
     <div className='border border-solid border-[rgba(6,18,36,0.1)] py-[22px] px-[18px] flex md:w-[308px] mobile:w-full rounded-xl bg-white gap-3 items-center'>
         <Image src={img} alt='' width={100} height={1} className='w-[45px]' />
