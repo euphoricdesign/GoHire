656280de3bf14b1144f97a7ca657088d43ae6b48
alejo saez <alejosaezgebicki@gmail.com>
2024-06-04 15:58:55 -0300

    se hizofuncionar el filtrado y ademas se agreo types para users

--- src/types/jobsTypes.ts
+++ src/types/jobsTypes.ts
@@ -8,10 +8,25 @@ export interface JobsData {
   time: string;
   timelapse: string;
   category: string;
+  user: UserData
 }
 export interface JobsPostData {
   title: string;
   description: string;
   imgUrl: string;
   category: string;
+}
+
+export interface UserData{
+id:string,
+name:string,
+lastName:string,
+dni:string,
+country:string,
+city:string,
+birthdate:Date,
+bio:string,
+availableToWork:boolean,
+professionalRate:string,
+newMember:boolean
 }
