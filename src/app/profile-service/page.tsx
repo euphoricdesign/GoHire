6f4de9055367ee5f873aa1aaea4c42a6d898b085
alejo saez <alejosaezgebicki@gmail.com>
2024-06-04 18:52:18 -0300

    Autentication de auth0, se agregaron enrutados

---
+++ src/app/profile-service/page.tsx
@@ -0,0 +1,34 @@
+import { getSession } from '@auth0/nextjs-auth0';
+import { GetServerSideProps } from 'next';
+
+type UserProfile = {
+  picture?: string;
+  name?: string;
+  email?: string;
+};
+
+type ProfileServerProps = {
+  user?: UserProfile;
+};
+
+export default function ProfileServer({ user }: ProfileServerProps) {
+  return (
+    user && (
+      <div>
+        <img src={user.picture ?? ''} alt={user.name ?? 'User'} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context.req, context.res);
+
+  return {
+    props: {
+      user: session?.user || null,
+    },
+  };
+};
