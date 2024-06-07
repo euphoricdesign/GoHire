9c7c6139deb044e236d5faa04cba92d7287dcd6b
Merlina <noahchukita@gmail.com>
2024-06-06 21:27:11 -0300

    Cambios en jobTypes

--- src/types/jobsTypes.ts
+++ src/types/jobsTypes.ts
@@ -1,23 +1,35 @@
-import { UserData } from "./userTypes";
-
-
-
-
+// recibir desde el back
 export interface JobsData {
   id: string;
   title: string;
   description: string;
-  imgUrl: string;
+  image: string;
   date: string;
   time: string;
   timelapse: string;
   category: string;
   user: UserData
 }
+
 export interface JobsPostData {
   title: string;
   description: string;
-  imgUrl: string;
   category: string;
+  location: string;
+  remoteWork: boolean;
+  file?: File; // Asegúrate de que image sea opcional si no siempre se envía.
 }
 
+export interface UserData{
+id:string,
+name:string,
+lastName:string,
+dni:string,
+country:string,
+city:string,
+birthdate:Date,
+bio:string,
+availableToWork:boolean,
+professionalRate:string,
+newMember:boolean
+}
