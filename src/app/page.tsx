ff734ee8de4d4273f58246da2a2f2696bd33207b
Merlina <noahchukita@gmail.com>
2024-06-08 14:04:28 -0300

    Maquetado secciones pago de publicidad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function MainPage() {
               <span className="text-[18px]">Stand out and win more work</span>
               <p className={`${merriweather.className} text-[27px] mt-[10px] max-w-[600px]`}>Ads are a proven way to help you get hired at any stage of your career.</p>
               <button className="mt-[35px] mb-5 text-sm border-none w-[150px] p-2.5 h-10 rounded text-white font-medium bg-[#3C65F5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0 md:block hidden" onClick={() => window.location.href = '/formJobs'}>
-                Show me how
+                <a href="/ads">Show me how</a>
               </button>
             </div>
               <Image className="w-[240px] justify-self-end" src={Cohete} alt="" />
