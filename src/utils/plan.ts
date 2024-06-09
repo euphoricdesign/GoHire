c678d3c2bfc85982357481ac1966fa49b7ac9688
Merlina <noahchukita@gmail.com>
2024-06-09 17:23:29 -0300

    planes pagos

---
+++ src/utils/plan.ts
@@ -0,0 +1,23 @@
+const plans = [
+    {
+      id: 1,
+      title: 'Plan Semanal',
+      quantity: 7, // 7 días
+      unitPrice: 5000,
+      currencyId: 'ARS',
+    },
+    {
+      id: 2,
+      title: 'Plan Quincenal',
+      quantity: 15, // 15 días
+      unitPrice: 8000,
+      currencyId: 'ARS',
+    },
+    {
+      id: 3,
+      title: 'Plan Mensual',
+      quantity: 30, // 30 días
+      unitPrice: 15000,
+      currencyId: 'ARS',
+    },
+];
