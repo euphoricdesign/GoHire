96fb714405dd0c84260b72bc477eb80eee7ae2c4
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-06-11 08:48:18 -0300

    dt-integration

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -123,13 +123,13 @@ const Navbar: React.FC = () => {
           </Link>
         </div>
         <button
-          className="mt-0 mb-5 text-sm border-none w-28 p-2.5 h-10 rounded text-white font-medium dark:bg-black bg-[#3C65F5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0 md:block hidden"
+          className="mt-0 mb-5 text-sm border-none w-28 p-2.5 h-10 rounded text-white font-medium bg-[#3C65F5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0 md:block hidden"
           onClick={() => (window.location.href = "/formJobs")}
         >
           Post a job
         </button>
+        <ThemeButtons />
       </div>
-      <ThemeButtons />
     </header>
   );
 };
