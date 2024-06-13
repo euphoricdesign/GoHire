8509356a98c05e2249959eaa2531f381b362ce12
alejo saez <alejosaezgebicki@gmail.com>
2024-06-13 17:38:19 -0300

    se agrego botones al dashboard de admin para poder navegar(falta la navegacion)

---
+++ src/components/CategoryAdmin/CategoryAdmin.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ButtonCategory: React.FC = () => {
+  return (
+    <button
+      className="mt-0 mb-5 text-sm border-none w-26 p-2.5 h-10 rounded text-white font-medium bg-[#3C65F5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0 md:block hidden"
+    >
+      Create Category
+    </button>
+  );
+};
+
+export default ButtonCategory;
