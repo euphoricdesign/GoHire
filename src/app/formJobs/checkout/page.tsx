68cc7d61c68c412f79e28ab2bc202b34ba6fd9c2
Merlina <noahchukita@gmail.com>
2024-06-15 17:29:47 -0300

    Ajustando ubicación loader

--- src/app/formJobs/checkout/page.tsx
+++ src/app/formJobs/checkout/page.tsx
@@ -92,7 +92,7 @@ const Checkout: React.FC = () => {
                     </div>
                 </div>
             ) : (
-                <div className="w-full flex flex-row gap-2 justify-center items-center">
+                <div className="w-full flex flex-row gap-2 justify-center items-center mt-[100px]">
                     <div className="w-4 h-4 rounded-full bg-[#3C65F5] animate-bounce"></div>
                     <div className="w-4 h-4 rounded-full bg-[#3C65F5] animate-bounce [animation-delay:-.3s]"></div>
                     <div className="w-4 h-4 rounded-full bg-[#3C65F5] animate-bounce [animation-delay:-.5s]"></div>
