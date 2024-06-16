505faee9e1eeb45c2fcb0e6a5cd4851eb6dbcb27
Merlina <noahchukita@gmail.com>
2024-06-16 19:18:27 -0300

    Creando blog page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         bannerBg:
           'url("https://jthemes.com/themes/wp/jobbox/wp-content/themes/jobbox/assets/imgs/banner.png")',
+        grayBg: 'url("https://jthemes.com/themes/wp/jobbox/wp-content/themes/jobbox/assets/imgs/page/blog/bg-black-trans.png")',
+        bgBlog1: 'url("https://i.pinimg.com/originals/4a/9d/0e/4a9d0e50c2d9e43135e92d47f57b3c24.jpg")',
+        bgBlog2: 'url("https://i.pinimg.com/564x/cd/5b/29/cd5b2982e5bc61f0b73e8e1e09830388.jpg")',
+        bgBlog3: 'url("https://i.pinimg.com/originals/00/24/8d/00248d10b5184a57ce3c522533c6397f.jpg")'
       },
       screens: {
         // Breakpoints predefinidos
