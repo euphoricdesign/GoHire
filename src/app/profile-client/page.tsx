a424e294d5673c53aa06910a412d173c9ef20c51
Merlina <noahchukita@gmail.com>
2024-06-17 13:21:43 -0300

    Cambiando loaders

--- src/app/profile-client/page.tsx
+++ src/app/profile-client/page.tsx
@@ -5,7 +5,11 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 export default function ProfileClient() {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div className="w-full flex flex-row gap-2 justify-center items-center mb-[60px]">
+      <div className="w-4 h-4 rounded-full bg-[#3C65F5] animate-bounce"></div>
+      <div className="w-4 h-4 rounded-full bg-[#3C65F5] animate-bounce [animation-delay:-.3s]"></div>
+      <div className="w-4 h-4 rounded-full bg-[#3C65F5] animate-bounce [animation-delay:-.5s]"></div>
+    </div>
   if (error) return <div>{error.message}</div>;
 
   return (
