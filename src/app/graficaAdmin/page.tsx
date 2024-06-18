ae1b6e29686d07065630f0617f99affe4d27232e
alejo saez <alejosaezgebicki@gmail.com>
2024-06-18 13:28:35 -0300

    se modifico el mook del front para los graficos

--- src/app/graficaAdmin/page.tsx
+++ src/app/graficaAdmin/page.tsx
@@ -4,13 +4,13 @@ import { NextPage } from 'next';
 import GraficaAdmin from '@/components/GraficaAdmin/GraficaAdmin';
 
 const GraficaPage: NextPage = () => {
-  const earningsData = [5000, 7000, 8000, 6000, 7500, 8500, 9000, 9500, 7000, 8000, 8500, 9000];
-  const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  const earningsData = [2000, 3000, 4500, 6000, 7500, 1500, 9000, 10000, 7000, 9500, 10500, 9000];
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-6 text-center">Ganancias Mensuales</h1>
+      <div className="container mx-auto p-4 mt-20">
+        <h1 className="text-3xl font-bold mb-6 text-center">Monthly Earnings</h1>
         <GraficaAdmin data={earningsData} labels={labels} />
       </div>
     </div>
