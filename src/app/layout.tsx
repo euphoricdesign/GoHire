75b6aebca349533d1ac499bf6548ab4e000ec474
Merlina <noahchukita@gmail.com>
2024-06-19 19:14:36 -0300

    Descomentando footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
             <AuthProvider>
               <Navbar />
               {children}
-              {/* <Footer /> */}
+              <Footer />
             </AuthProvider>
           </UserProvider>
         </StoreProvider>
