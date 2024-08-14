e5d0626cb01447c2cbe01fded0374e481dc7a978
Merlina <noahchukita@gmail.com>
2024-08-14 16:56:36 -0300

    style(responsive): mejora la responsividad en seciones Ads y Checkout

--- src/app/ads/page.tsx
+++ src/app/ads/page.tsx
@@ -8,12 +8,12 @@ import { SiSpotlight } from "react-icons/si";
 
 const Ads = () => {
   return (
-    <div className='mobile:px-[30px] my-[170px] md:px-[124px]'>
-        <div className='flex mobile:flex-col-reverse md:flex-row justify-between'>
+    <div className='mobile:px-[30px] mobile:mt-[140px] md:mt-[170px] mb-[100px] md:px-[124px]'>
+        <div className='flex mobile:flex-col-reverse md:flex-row justify-between mobile:gap-[34px] md:gap-0'>
             <div className='mobile:w-full md:w-[600px] text-[#05264E]'>
                 <h1 className='mobile:text-[30px] mobile:w-[300px] md:w-full md:text-5xl font-bold'>Gain work opportunities with ads</h1>
                 <p className='mt-[2.1rem] mobile:w-[300px] md:w-full mobile:text-base md:text-[1.25rem]'>Choosing the recommended budget means that your offer will receive better visibility and will be displayed more often in search results, making it easier for people looking for a job like yours to apply.</p>
-                <div className='mt-[2.1rem] flex mobile:flex-col md:items-center gap-[20px] mobile:w-[300px] md:w-full'>
+                <div className='mt-[2.1rem] flex mobile:flex-col md:flex-row md:items-center gap-[20px] mobile:w-[300px] md:w-full'>
                     <button
                         className="text-base border-none w-40 p-2.5 h-12 rounded text-white font-medium bg-[#3C65F5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0"
                         onClick={() => (window.location.href = "/formJobs")}>
@@ -25,10 +25,10 @@ const Ads = () => {
             <Image src={Grow} alt='' className='w-[550px]' />
         </div>
 
-        <h2 className='mobile:text-[30px] md:text-4xl font-bold text-[#05264E] mt-[4.5rem]'>Benefits of using spotlight posts to reach your work goal</h2>
+        <h2 className='mobile:text-[25px] md:text-4xl font-bold text-[#05264E] mt-[4.5rem]'>Benefits of using spotlight posts to reach your work goal</h2>
 
         <div className='mt-[4.5rem] mobile:block md:flex justify-between'>
-            <div className="card mobile:w-full md:w-[380px] mobile:mb-[15px] md:mb-0 ">
+            <div className="card mobile:w-full md:w-[380px] mobile:mb-[55px] md:mb-0 ">
                 <div className="content">
                     <FaHighlighter />
                     <p className="para">
@@ -36,7 +36,7 @@ const Ads = () => {
                     </p>
                 </div>
             </div>
-            <div className="card mobile:w-full md:w-[380px] mobile:mb-[15px] md:mb-0 ">
+            <div className="card mobile:w-full md:w-[380px] mobile:mb-[55px] md:mb-0 ">
                 <div className="content">
                     <SiSpotlight />
                     <p className="para">
@@ -44,7 +44,7 @@ const Ads = () => {
                     </p>
                 </div>
             </div>
-            <div className="card mobile:w-full md:w-[380px] mobile:mb-[15px] md:mb-0 ">
+            <div className="card mobile:w-full md:w-[380px] mobile:mb-[55px] md:mb-0 ">
                 <div className="content">
                     <FaRegLightbulb />
                     <p className="para">
@@ -55,7 +55,7 @@ const Ads = () => {
         </div>
 
         <div>
-            <h2 className='mobile:text-[30px] md:text-4xl font-bold text-[#05264E] mt-[4.5rem]'>Why do I need to use ads?</h2>
+            <h2 className='mobile:text-[25px] md:text-4xl font-bold text-[#05264E] mt-[4.5rem]'>Why do I need to use ads?</h2>
             <p className='text-[#05264E] mt-[2.1rem] mobile:text-base md:text-[1.25rem]'>Advertising is an optional choice; proposals can be submitted without it. However, ads provide increased visibility, helping you secure desired projects and streamline your workflow for maximized earnings on high-quality endeavors.</p>
             <button
                 className="mt-[2.1rem] text-base border-none w-40 p-2.5 h-12 rounded text-white font-medium bg-[#3C65F5] cursor-pointer transition-opacity duration-300 ease-in-out opacity-100 hover:opacity-80 md:mb-0"
