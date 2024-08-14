023de1726196160733d5f91a2f1de30147f805b9
Merlina <noahchukita@gmail.com>
2024-08-13 22:15:46 -0300

    style(responsive): ajusta estilos para hacer responsive el componente BannerCategory

--- src/components/BannerCategory/BannerCategory.tsx
+++ src/components/BannerCategory/BannerCategory.tsx
@@ -26,7 +26,7 @@ const BannerCategory: React.FC<BannerCategoryProps> = ({
 
   return (
     <div className="bg-bannerBg bg-center flex flex-col justify-center items-center p-[40px] rounded-xl mt-[100px]">
-      <h2 className="text-[28px] text-[#05264E] font-[700] z-10">
+      <h2 className="mobile:text-[22px] md:text-[28px] text-[#05264E] font-[700] z-10">
         <span className="text-[#3C65F5] relative spanAfterSm">{count || 0} Jobs</span> Available Now
       </h2>
       <p className="leading-custom text-sm font-medium text-[#66789C] text-center mt-[12px]">
@@ -34,42 +34,48 @@ const BannerCategory: React.FC<BannerCategoryProps> = ({
         atque delectus molestias quis
       </p>
       <div className="mt-[40px]">
-        <div className="relative w-[580px] bg-white py-[12px] px-[16px] flex gap-4 items-center rounded shadow-custom">
+        <div className="relative md:w-[580px] bg-white py-[12px] px-[16px] flex gap-4 items-center rounded shadow-custom">
           <div className="flex-1 relative">
-            <div className="flex items-center gap-[15px] text-[#747474]">
-              <FaBriefcase className="text-[40px]" />
-              <select
-                id="category"
-                value={selectedCategory}
-                onChange={handleCategoryChange}
-                className="py-[0.6rem] px-4 focus:outline-none w-full">
-                <option value="">Filter by category</option>
-                {isLoading && <option>Loading...</option>}
-                {isError && <option>Error loading categories</option>}
-                {categories?.categoryReturn &&
-                  categories.categoryReturn.map((category: string, index: number) => (
-                    <option key={index} value={category}>
-                      {category}
-                    </option>
-                  ))}
-              </select>
+            <div className="flex mobile:flex-col md:flex-row items-center gap-[15px] text-[#747474]">
+              <FaBriefcase className="text-[40px] mobile:hidden md:block" />
+              <div className="w-full mobile:flex items-center md:block">
+                <FaBriefcase className="text-[18px] md:hidden mobile:block" />
+                <select
+                  id="category"
+                  value={selectedCategory}
+                  onChange={handleCategoryChange}
+                  className="py-[0.6rem] px-4 focus:outline-none w-full">
+                  <option value="">Filter by category</option>
+                  {isLoading && <option>Loading...</option>}
+                  {isError && <option>Error loading categories</option>}
+                  {categories?.categoryReturn &&
+                    categories.categoryReturn.map((category: string, index: number) => (
+                      <option key={index} value={category}>
+                        {category}
+                      </option>
+                    ))}
+                </select>
+              </div>
 
-              <FaSearchLocation className="text-[40px]" />
-              <select
-                id="city"
-                value={selectedCountry}
-                onChange={handleCountryChange}
-                className="py-[0.6rem] px-4 focus:outline-none w-full">
-                <option value="">Filter by Location</option>
-                {isLoading && <option>Loading...</option>}
-                {isError && <option>Error loading categories</option>}
-                {categories?.locationReturn &&
-                  categories.locationReturn.map((location: string, index: number) => (
-                    <option key={index} value={location}>
-                      {location}
-                    </option>
-                  ))}
-              </select>
+              <FaSearchLocation className="text-[40px] mobile:hidden md:block" />
+             <div className="w-full mobile:flex items-center md:block">
+                <FaSearchLocation className="text-[18px] md:hidden mobile:block" />
+                <select
+                    id="city"
+                    value={selectedCountry}
+                    onChange={handleCountryChange}
+                    className="py-[0.6rem] px-4 focus:outline-none w-full">
+                    <option value="">Filter by Location</option>
+                    {isLoading && <option>Loading...</option>}
+                    {isError && <option>Error loading categories</option>}
+                    {categories?.locationReturn &&
+                      categories.locationReturn.map((location: string, index: number) => (
+                        <option key={index} value={location}>
+                          {location}
+                        </option>
+                      ))}
+                  </select>
+             </div>
             </div>
           </div>
         </div>
